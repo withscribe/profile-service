68145c25d42678879bc5acf08e3b27face12404f
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2018-11-07 20:43:19 -0500

    refactored query/mutation functions to use fat arrow function, added communities support queries and updated story with new fields

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,243 +1,264 @@
 const { verifyToken } = require('../utils');
 
-async function createProfile(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.createProfile({
-        account_id: args.accountId,
-        firstName: args.firstName,
-        lastName: args.lastName,
-        dateOfBirth: args.dob,
-        occupation: args.occupation,
-        employer: {
-            create: {
-                orgName: args.orgName,
-                country: args.country,
-                address: args.address,
-                orgType: args.orgType,
-            }
-        },
-    })
-}
-
-async function registerProfile(_, args, context, info) {
-    return context.prisma.createProfile({
-        account_id: args.accountId,
-        userName: args.userName
-    })
-}
-async function updateProfileCreate(_, args, context, info) {
-    const payload = verifyToken(context);
-
-    const profile = await context.prisma.profile({ account_id: payload.accountId })
-
-    if(args.userName != profile.userName) {
-        const exists = await context.prisma.$exists.profile({ userName: args.userName })
-
-        if(exists == null) {
-            profile.userName = args.userName
-        } else {
-            throw new Error("Username has already been taken")
-        }
-    }
-    return updatedProfile = await context.prisma.updateProfile({
-        where: {
-            id: profile.id
-        },
-        data: {
-            userName: profile.userName,
-            firstName: args.firstName,
-            lastName: args.lastName,
-            dateOfBirth: args.dob,
-            occupation: args.occupation,
-        }
-    })
-}
-
-function updateProfileConnect(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.updateProfile({
-        where: {
-            account_id: payload.accountId
-        },
-        data: {
-            userName: args.userName,
-            firstName: args.firstName,
-            lastName: args.lastName,
-            dateOfBirth: args.dob,
-            occupation: args.occupation,
-            employer: {
-                connect: {
-                    id: args.employerId
-                }
-            },
-        }
-    })
-}
-
-function removeProfile(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.deleteProfile({ id: args.id })
-}
-
-function createOrganization(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.createOrganization({
+createProfile = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.createProfile({
+    account_id: args.accountId,
+    firstName: args.firstName,
+    lastName: args.lastName,
+    dateOfBirth: args.dob,
+    occupation: args.occupation,
+    employer: {
+      create: {
         orgName: args.orgName,
-        address: args.address,
         country: args.country,
-        orgType: args.orgType
-    })
+        address: args.address,
+        orgType: args.orgType,
+      }
+    },
+  })
 }
 
-function updateOrganization(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.createOrganization({
-        where: {
-            id: args.orgId
-        },
-        data: {
-            orgName: args.orgName,
-            address: args.adress,
-            country: args.country,
-            orgType: args.orgType
-        },
-    })
+registerProfile = async (_, args, context, info) => {
+  return await context.prisma.createProfile({
+    account_id: args.accountId,
+    userName: args.userName
+  })
 }
 
-function removeOrganization(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.deleteOrganization({ id: args.id })
+updateProfileCreate = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+
+  const profile = await context.prisma.profile({ account_id: payload.accountId })
+
+  if(args.userName != profile.userName) {
+    const exists = await context.prisma.$exists.profile({ userName: args.userName })
+
+    if(exists == null) {
+      profile.userName = args.userName
+    } else {
+      throw new Error("Username has already been taken")
+    }
+  }
+  return updatedProfile = await context.prisma.updateProfile({
+    where: {
+      id: profile.id
+    },
+    data: {
+      userName: profile.userName,
+      firstName: args.firstName,
+      lastName: args.lastName,
+      dateOfBirth: args.dob,
+      occupation: args.occupation,
+    }
+  })
 }
 
-function addNewEmployerToProfile(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.updateProfile({
-        where: {
-            account_id: payload.accountId
-        },
-        data: {
-            employer: {
-                create: {
-                    orgName: args.employer.orgName,
-                    address: args.employer.address,
-                    country: args.employer.country,
-                    orgType: args.employer.address
-                }
-            }
+updateProfileConnect = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.updateProfile({
+    where: {
+      account_id: payload.accountId
+    },
+    data: {
+      userName: args.userName,
+      firstName: args.firstName,
+      lastName: args.lastName,
+      dateOfBirth: args.dob,
+      occupation: args.occupation,
+      employer: {
+        connect: {
+            id: args.employerId
         }
-    })
+      },
+    }
+  })
 }
 
-function addEmployerToProfile(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.updateProfile({
-        where: {
-            account_id: payload.accountId
-        },
-        data: {
-            employer: {
-                connect: {
-                    id: args.employerId
-                }
-            }
+removeProfile = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.deleteProfile({ id: args.id })
+}
+
+createOrganization = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.createOrganization({
+    orgName: args.orgName,
+    address: args.address,
+    country: args.country,
+    orgType: args.orgType
+  })
+}
+
+updateOrganization = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.createOrganization({
+    where: {
+      id: args.orgId
+    },
+    data: {
+      orgName: args.orgName,
+      address: args.adress,
+      country: args.country,
+      orgType: args.orgType
+    },
+  })
+}
+
+removeOrganization = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.deleteOrganization({ id: args.id })
+}
+
+addNewEmployerToProfile = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.updateProfile({
+    where: {
+      account_id: payload.accountId
+    },
+    data: {
+      employer: {
+        create: {
+          orgName: args.employer.orgName,
+          address: args.employer.address,
+          country: args.employer.country,
+          orgType: args.employer.address
         }
-    })
+      }
+    }
+  })
 }
 
-function addStoriesToProfile(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.updateProfile({
-        where: {
-            account_id: accountId
-        },
-        data: {
-            stories: {
-                connect: [
-                    { id: args.storyIds }
-                ]
-            }
+addEmployerToProfile = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.updateProfile({
+    where: {
+      account_id: payload.accountId
+    },
+    data: {
+      employer: {
+        connect: {
+            id: args.employerId
         }
-    })
+      }
+    }
+  })
 }
 
-async function updateFlaggedCount(_, args, context, info) {
-    const payload = verifyToken(context);
-    const profile = await context.prisma.profile({ id: args.profileId })
-
-    profile.flaggedStories = profile.flaggedStories + 1;
-
-    return context.prisma.updateProfile({
-        where: {
-            id: args.profileId
-        },
-        data: {
-            flaggedStories: profile.flaggedStories
-        }
-    })
+addStoriesToProfile = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.updateProfile({
+    where: {
+      account_id: accountId
+    },
+    data: {
+      stories: {
+        connect: [
+          { id: args.storyIds }
+        ]
+      }
+    }
+  })
 }
 
-async function updatePublishedCount(_, args, context, info) {
-    const payload = verifyToken(context);
-    const profile = await context.prisma.profile({ id: args.profileId })
+updateFlaggedCount = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  const profile = await context.prisma.profile({ id: args.profileId })
 
-    profile.storiesPublished = profile.storiesPublished + 1;
+  profile.flaggedStories = profile.flaggedStories + 1;
 
-    return context.prisma.updateProfile({
-        where: {
-            id: args.profileId
-        },
-        data: {
-            storiesPublished: profile.storiesPublished
-        }
-    })
+  return await context.prisma.updateProfile({
+    where: {
+      id: args.profileId
+    },
+    data: {
+      flaggedStories: profile.flaggedStories
+    }
+  })
 }
 
-async function updateReviewedCount(_, args, context, info) {
-    const payload = verifyToken(context);
-    const profile = await context.prisma.profile({ id: args.profileId })
+updatePublishedCount = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  const profile = await context.prisma.profile({ id: args.profileId })
 
-    profile.storiesReviewed = profile.storiesReviewed + 1;
+  profile.storiesPublished = profile.storiesPublished + 1;
 
-    return context.prisma.updateProfile({
-        where: {
-            id: args.profileId
-        },
-        data: {
-            storiesReviewed: profile.storiesReviewed
-        }
-    })
+  return context.prisma.updateProfile({
+    where: {
+      id: args.profileId
+    },
+    data: {
+      storiesPublished: profile.storiesPublished
+    }
+  })
 }
 
-async function addLikedStory(_, args, context, info) {
-    const payload = verifyToken(context);
+updateReviewedCount = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  const profile = await context.prisma.profile({ id: args.profileId })
 
-    const profile = await context.prisma.profile({ account_id: payload.accountId })
-    const like = await context.prisma.createLikes({ guid: args.storyId + profile.id })
+  profile.storiesReviewed = profile.storiesReviewed + 1;
+
+  return context.prisma.updateProfile({
+    where: {
+      id: args.profileId
+    },
+    data: {
+      storiesReviewed: profile.storiesReviewed
+    }
+  })
+}
+
+addLikedStory = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+
+  const profile = await context.prisma.profile({ account_id: payload.accountId })
+  const like = await context.prisma.createLikes({ guid: args.storyId + profile.id })
+
+  return await context.prisma.updateProfile({
+    where: {
+      id: profile.id
+    },
+    data: {
+      storiesLiked: { connect: { id: like.id } }
+    }
+  })
+}
+
+removeLikedStory = (_, args, context, info) => {
+  const payload = verifyToken(context);
+
+  const profile = await context.prisma.profile({ account_id: payload.accountId })
+  const like = await context.prisma.createLikes({ guid: args.storyId + profile.id })
+
+  return await context.prisma.updateProfile({
+    where: {
+      id: profile.id
+    },
+    data: {
+      storiesLiked: { delete: { id: like.id } }
+    }
+  })
+}
+
+setCommunityToProfile = async (_, args, context, info) => {
+    const payload = verifyToken(context)
+    const profile = await context.prisma.profile({ id: args.id })
+
+    const updatedList = community.communitiesIds
+    updatedList.push(args.communityId)
 
     return await context.prisma.updateProfile({
-        where: {
-            id: profile.id
-        },
-        data: {
-            storiesLiked: { connect: { id: like.id } }
+      where: {
+        id: args.id
+      },
+      data: {
+        communitiesIds: {
+          set: updatedList
         }
+      }
     })
-}
-
-async function removeLikedStory(_, args, context, info) {
-    const payload = verifyToken(context);
-
-    const profile = await context.prisma.profile({ account_id: payload.accountId })
-    const like = await context.prisma.createLikes({ guid: args.storyId + profile.id })
-
-    return await context.prisma.updateProfile({
-        where: {
-            id: profile.id
-        },
-        data: {
-            storiesLiked: { delete: { id: like.id } }
-        }
-    })
+    // add profile fragment
 }
 
 module.exports = { 
@@ -256,5 +277,6 @@ module.exports = {
     updatePublishedCount,
     updateFlaggedCount,
     addLikedStory,
-    removeLikedStory    
+    removeLikedStory,
+    setCommunityToProfile    
 }
--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,82 +1,89 @@
 const { verifyToken } = require('../utils');
 
-async function profileById(_, args, context, info) {
-    const payload = verifyToken(context);
-    return profile = await context.prisma.profile({ id: args.id })
+profileById = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return profile = await context.prisma.profile({ id: args.id })
 }
 
-function profileByAccountId(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.profile({ account_id: args.accountId })
+profileByAccountId = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.profile({ account_id: args.accountId })
 }
 
-function profileByUsername(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.profiles({
-        where: {
-            OR: [
-                { userName_contains: args.userName }
-            ]
-        }
-    })
-}
-function orgById(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.organization({ id: args.id })
+profileByUsername = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.profiles({
+    where: {
+      OR: [
+        { userName_contains: args.userName }
+      ]
+    }
+  })
 }
 
-function searchProfiles(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.profiles({
-        where: {
-            OR: [
-                { userName_contains: args.searchString },
-                { firstName_contains: args.searchString },
-                { lastName_contains: args.searchString },
-                { dateOfBirth_contains: args.searchString },
-                { occupation_contains: args.searchString }
-            ],
-        },
-    })
+orgById = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.organization({ id: args.id })
 }
 
-function searchOrganizations(_, args, context, info)  {
-    const payload = verifyToken(context);
-    return context.prisma.organizations({
-        where: {
-            OR: [
-                { orgName_contains: args.searchString },
-                { orgType_contains: args.searchString },
-                { address_contains: args.searchString },
-                { country_contains: args.searchString },
-            ]
-        }
-    })
+searchProfiles = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return context.prisma.profiles({
+    where: {
+      OR: [
+        { userName_contains: args.searchString },
+        { firstName_contains: args.searchString },
+        { lastName_contains: args.searchString },
+        { dateOfBirth_contains: args.searchString },
+        { occupation_contains: args.searchString }
+      ],
+    },
+  })
 }
 
-function allProfiles(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.profiles(_)
-
+searchOrganizations = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.organizations({
+    where: {
+      OR: [
+        { orgName_contains: args.searchString },
+        { orgType_contains: args.searchString },
+        { address_contains: args.searchString },
+        { country_contains: args.searchString },
+      ]
+    }
+  })
 }
-function allOrganizations(_, args, context, info) {
-    const payload = verifyToken(context);
-    return context.prisma.organizations(_)
+
+allProfiles = async(_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.profiles({})
 }
 
+allOrganizations = async (_, args, context, info) => {
+  const payload = verifyToken(context);
+  return await context.prisma.organizations({})
+}
 
-async function userNameExists(_, args, context, info) {
-    return await context.prisma.profile({ userName: args.userName })
+userNameExists = async (_, args, context, info) => {
+  return await context.prisma.profile({ userName: args.userName })
+}
+
+communitiesMembers = (_, args, context, info) => {
+  const payload = verifyToken(context)
+  return context.prisma.profiles({where: { id_in: args.membersIds }})
 }
 
 module.exports = {
-    searchProfiles,
-    allProfiles,
-    profileById,
-    profileByAccountId,
-    profileByUsername,
-    allOrganizations,
-    searchOrganizations,
-    orgById,
-    userNameExists
+  searchProfiles,
+  allProfiles,
+  profileById,
+  profileByAccountId,
+  profileByUsername,
+  allOrganizations,
+  searchOrganizations,
+  orgById,
+  userNameExists,
+  profilesByCommunityId,
+  communitiesMembers
 }
