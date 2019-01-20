f312ebbdc95481b7284cf33db855a5e46ce37634
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-19 19:56:20 -0500

    cleaning up files of useless comments

--- src/fragments/OrganizationFragment.js
+++ src/fragments/OrganizationFragment.js
@@ -1,6 +1,3 @@
-// Author: Austin Howlett
-// Description: Fragments ensure that, if requested, all of these fields will be returned
-
 const organizationFragment = `
   fragment organizationFragment on Organization {
      id
