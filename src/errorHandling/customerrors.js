0ba9f67aaa3729e8dde9af256c10df44b924980a
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-01-19 19:45:57 -0500

    added context as a constructor argument to AuthError

--- src/errorHandling/customerrors.js
+++ src/errorHandling/customerrors.js
@@ -1,33 +1,34 @@
 class AuthError extends Error {
-    constructor(message, ...params) {
-      super(...params)
+  constructor(message, context, ...params) {
+    super(...params)
 
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, AuthError)
-      }
-
-      this.message = message
-      this.date = new Date()
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, AuthError)
     }
+
+    this.message = message
+    this.context = context
+    this.date = new Date()
+  }
 }
 
 class LoginError extends Error {
-    constructor(message, info, context, ...params) {
-      super(...params)
+  constructor(message, info, context, ...params) {
+    super(...params)
 
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, LoginError)
-      }
-
-      this.message = message
-      this.info = info
-      this.context = context
-      this.date = new Date()
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, LoginError)
     }
+
+    this.message = message
+    this.info = info
+    this.context = context
+    this.date = new Date()
+  }
 }
 
 
 module.exports = {
-    AuthError,
-    LoginError,
+  AuthError,
+  LoginError,
 }
